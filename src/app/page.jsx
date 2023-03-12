ad1f50b3d961fd34beefd496060fb76e29877e8d
francis omondi <francisomondi17@gmail.com>
2023-03-12 13:00:53 +0300

    next js and sanity configured successfully

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+
 import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
@@ -52,12 +52,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
+
         </a>
 
         <a
@@ -66,10 +61,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
         </a>
 
         <a
@@ -78,12 +69,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+
         </a>
       </div>
     </main>
